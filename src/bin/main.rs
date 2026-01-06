d1bca2cb00d895ae2112bf746ca78e87c96f7881
Grmislav <83614877+TomislavGrgic@users.noreply.github.com>
2026-01-06 20:16:11 +0100

    First Commit

---
+++ src/bin/main.rs
@@ -0,0 +1,61 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+#![deny(clippy::large_stack_frames)]
+
+use esp_println::println;
+use esp_hal::{
+    clock::CpuClock,
+    main,
+    time::{Duration, Instant, Rate},
+    i2c::master,
+};
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[allow(
+    clippy::large_stack_frames,
+    reason = "it's not unusual to allocate larger buffers etc. in main"
+)]
+#[main]
+fn main() -> ! {
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let config = master::Config::default().with_frequency(Rate::from_khz(400));
+    let mut i2c = master::I2c::new(peripherals.I2C0, config)
+            .unwrap()
+            .with_sda(peripherals.GPIO23)
+            .with_scl(peripherals.GPIO15);
+
+    loop {
+        let delay_start = Instant::now();
+        while delay_start.elapsed() < Duration::from_millis(500) {}
+        println!("Status ---> {:?}", read_temp(&mut i2c));
+    }
+}
+
+
+fn read_temp(i2c: &mut master::I2c<impl esp_hal::DriverMode>) -> Result<f32, master::Error> {
+    i2c.write(0x44, &[0x24, 0x0B])?;
+    
+    let delay_start = Instant::now();
+    while delay_start.elapsed() < Duration::from_millis(20) {}
+
+    let mut read_buffer = [0u8; 6];
+
+    i2c.read(0x44, &mut read_buffer)?;
+
+    println!("array {:?}", read_buffer);
+
+    Ok(32.5)
+}
